ad585c0daa1bfd48da21a2ee9c660c6e5da72721
jesg <savagejas@gmail.com>
2014-04-21 19:27:28 -0700

    format

--- src/test/java/jesg/LinkTest.java
+++ src/test/java/jesg/LinkTest.java
@@ -20,42 +20,41 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
 public class LinkTest {
 	private static final Pattern COMMA = Pattern.compile(",");
-	
-    private WebDriver driver;
-    private Wait<WebDriver> wait;
-    
-	@DataProvider(name="links")
-	public Object[][] getLinks() throws IOException{
-        List<String> lines = FileUtils.readLines(new File("links.csv"));
-        Object[][] csvData = new Object[lines.size()][3];
-        
-        for(int i=0; i< lines.size(); i++){
-        	csvData[i] = COMMA.split(lines.get(i));
-        }
-        return csvData;
-	}
-	
-    @BeforeTest
-    public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
-    }
 
-    @AfterTest
-    public void tearDown() throws Exception {
-        driver.quit();
-    }
-    
-	@Test(dataProvider="links")
-	public void verifyLinks(String url, String link, String title){
-        driver.get(url);
-        driver.findElement(By.partialLinkText(link)).click();
-        wait.until(ExpectedConditions.titleIs(title));
-        assertEquals(title, driver.getTitle());
+	private WebDriver driver;
+	private Wait<WebDriver> wait;
+
+	@DataProvider(name = "links")
+	public Object[][] getLinks() throws IOException {
+		List<String> lines = FileUtils.readLines(new File("links.csv"));
+		Object[][] csvData = new Object[lines.size()][3];
+
+		for (int i = 0; i < lines.size(); i++) {
+			csvData[i] = COMMA.split(lines.get(i));
+		}
+		return csvData;
+	}
+
+	@BeforeTest
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 10);
+	}
+
+	@AfterTest
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test(dataProvider = "links")
+	public void verifyLinks(String url, String link, String title) {
+		driver.get(url);
+		driver.findElement(By.partialLinkText(link)).click();
+		wait.until(ExpectedConditions.titleIs(title));
+		assertEquals(title, driver.getTitle());
 	}
 
 }
